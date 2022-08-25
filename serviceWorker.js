6deb7e2629ce2a40518dc225c952119a3a006bd3
cckuang63196318 <48376750+cckuang63196318@users.noreply.github.com>
2022-08-25 23:23:25 +0800

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,29 @@
-const cacheVersion = "v2.06";
+const cacheVersion = "v2.07";
 const filesToCache = [
-
+  '/innohcm/',
+  './index.html',
+  './manifest.json',
+  './serviceWorker.js',
+  './innolux-offical.png',
+  './personIcon.png',
+  './favicon.ico',
+  './favicon72.png',
+  './favicon96.png',
+  './favicon144.png',
+  './favicon192.png',
+  './favicon512.png',
+  './certificate/CUXSG在職logo.jpg',
+  './certificate/CUXSG簽名檔.jpg',
+  './certificate/CUXSG離職logo.jpg',
+  './certificate/睿生在職logo.jpg',
+  './certificate/睿生簽名檔.jpg',
+  './certificate/睿生離職logo.jpg',
+  './certificate/群創在職logo.jpg',
+  './certificate/群創簽名檔.jpg',
+  './certificate/群創離職logo.jpg',
+  './certificate/群豐駿在職logo.jpg',
+  './certificate/群豐駿簽名檔.jpg',
+  './certificate/群豐駿離職logo.jpg'
 ];
 
 self.addEventListener("install", event => {
@@ -29,12 +52,26 @@ self.addEventListener('activate', event => {
 
 self.addEventListener("fetch", event => {  
   event.respondWith(
-    caches
-      .open(cacheVersion)
-      .then(cache => cache.match(event.request, { ignoreSearch: true }))
-      .then(response => {
-        console.log('[ServiceWorker] fetch', event.request);
-        return response || fetch(event.request);
-      }),
+    caches.match(event.request).then(function(response) {
+      if(response) {
+          console.log('fetch request: ' + event.request.url)
+          console.log('fetch response: ' + response.status)
+          return response;
+      } else {
+          return fetch(event.request).then(function(res) {
+              return caches.open(cacheVersion).then(function(cache) {
+                // cache common data
+                /*
+                if (/\/DomainController\/Common\/.+\/Get.+/.test(event.request.url)) {
+                  cache.put(event.request.url, res.clone());                  
+                }
+                */
+                return res;
+              })
+          }).catch(function(err) {
+            console.log('[ServiceWorker] fetch ' + err);
+          });
+      }
+    })
   );
 });
